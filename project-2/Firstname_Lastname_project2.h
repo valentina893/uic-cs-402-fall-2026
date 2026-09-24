09a29e974731de500efaecca3bd42c3804e19a1a
Valentina <zenit@Valentinas-MacBook-Air.local>
2026-09-24 14:47:58 -0500

    synced header file with prof block's header file in upstream repo

--- project-2/Firstname_Lastname_project2.h
+++ project-2/Firstname_Lastname_project2.h
@@ -11,8 +11,8 @@
 std::vector<unsigned int> birthday_attack_1(std::function<unsigned short(unsigned int)> hash_function);
 std::vector<unsigned int> birthday_attack_2(std::function<unsigned short(unsigned int)> hash_function);
 std::string merkle_commit(const std::vector<std::string>& list, std::function<std::string(std::string)> hash_function);
-std::vector<std::string> merkle_open_position(const std::vector<std::string>& list, std::function<std::string(std::string)> hash_function, const unsigned int i);
-int merkle_verify_position(const std::string root, const std::vector<std::string>& proof, std::function<std::string(std::string)> hash_function, const unsigned int i);
+std::vector<std::pair<std::string,std::string>> merkle_open_position(const std::vector<std::string>& list, std::function<std::string(std::string)> hash_function, const unsigned int i);
+int merkle_verify_position(const std::string root, const std::vector<std::pair<std::string, std::string>>& proof, std::function<std::string(std::string)> hash_function, const unsigned int i);
 int merkle_verify_full(const std::string root, const std::vector<std::string> list);
 
 
